c137ecfce14be423a7d67fc8e171e3df3e62697a
Henrik Neffe <henrikneffe@gmail.com>
2023-06-05 23:09:59 +0200

    all

--- Counter.py
+++ Counter.py
@@ -1,1 +1,28 @@
 
+DennisCounter = 0
+HenrikCounter = 0
+JulienCounter = 0
+OleCounter = 0
+with open('Chat.txt',encoding="utf8") as f:
+    contents = f.readlines()
+    #print(contents)
+
+for line in contents:
+    print(line)
+
+    if "13:37" in line and "1337" in line and "Gülli" in line: #Dennis Counter
+        DennisCounter += 1
+
+    if "13:37" in line and "1337" in line and "Henrik" in line: #Henrik Counter
+        HenrikCounter += 1
+
+    if "13:37" in line and "1337" in line and "Der Oberaal ALLA" in line: #Julien Counter
+        JulienCounter += 1
+
+    if "13:37" in line and "1337" in line and "Ole Mc Oleson" in line: #Ole Counter
+        OleCounter += 1
+        
+print("Dennis:",DennisCounter)
+print("Henrik:",HenrikCounter)
+print("Julien:",JulienCounter)
+print("Ole:",OleCounter)
